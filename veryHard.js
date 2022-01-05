a1e9192a98a08843e506b41d6f5bd9b931dd487b
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-01-05 17:02:48 -0500

    finished the coding challenge

--- veryHard.js
+++ veryHard.js
@@ -1,23 +1,23 @@
 var a = parseInt(window.prompt("Enter first number here: "));
-var b = parseInt(window.prompt("Enter first number here: "));
+var b = parseInt(window.prompt("Enter second number here: "));
 
-console.log("The sum of " + a + " and " + b + " is: " + sum());
-console.log("The difference of " + a + " and " + b + " is: " + diff());
-console.log("The multiplication of " + a + " and " + b + " is: " + multiply());
-console.log("The division of " + a + " and " + b + " is: " + divide());
+console.log("The sum of " + a + " and " + b + " is: " + sum(a,b));
+console.log("The difference of " + a + " and " + b + " is: " + diff(a,b));
+console.log("The multiplication of " + a + " and " + b + " is: " + multiply(a,b));
+console.log("The division of " + a + " and " + b + " is: " + divide(a,b));
 
-function sum() {
-    return a + b;
+function sum(a,b) {
+    return (a+b);
 }
 
-function diff() {
-    return a - b;
+function diff(a,b) {
+    return (a-b);
 }
 
-function multiply() {
-    return a * b;
+function multiply(a,b) {
+    return (a*b);
 }
 
-function divide() {
-    return a / b;
+function divide(a,b) {
+    return (a/b);
 }
